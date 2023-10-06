38155cf78993954d10d9c6503f484c331623b106
Leonardo Maia <94494441+leonardohmaia@users.noreply.github.com>
2023-10-05 23:36:26 -0300

    Create insertionsort.c

---
+++ insertionsort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    int i, chave, j;
+    for (i = 1; i < n; i++) {
+        chave = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > chave) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = chave;
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    insertionSort(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
